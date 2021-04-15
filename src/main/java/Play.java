2ab032c313b764522cc78059edfa0d7add06bf5b
Jemmy <jemmy_lin@wistron.com>
2021-04-15 09:27:03 +0800

    * Initial all kafka project

---
+++ src/main/java/Play.java
@@ -0,0 +1,79 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Play {
+    public static void main(String args[]) {
+        System.out.println("THis is test");
+
+//        producerSend();
+        consumerGet();
+//        streamConvert();
+    }
+
+    public static void producerSend() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.16.21:58424");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("linger.ms", 1);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++)
+            producer.send(
+                    new ProducerRecord("test", "ee"+Integer.toString(i), "ee"+Integer.toString(i)));
+
+        producer.close();
+    }
+
+    public static void consumerGet() {
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "192.168.16.21:58424");
+        props.setProperty("group.id", "test");
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("test", "topic"));
+
+        String receiveString = "";
+        while (true) {
+            if(receiveString.equals("exit"))
+                break;
+
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+                receiveString = record.value();
+            }
+        }
+    }
+
+    public static void streamConvert() {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "my-stream-processing-application");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.16.21:58424");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        builder.<String, String>stream("test").mapValues(value -> String.valueOf(value.length())).to("topic");
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+    }
+}
